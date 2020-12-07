4d516b99950d3accc1361459d8a9040807f466ab
Om Patel <oapatel@gmail.com>
2020-12-06 20:42:55 -0500

    added label and group fields in quickadd form

--- lib/components/QuickAddTimer.dart
+++ lib/components/QuickAddTimer.dart
@@ -17,6 +17,8 @@ class QuickAddTimerState extends State<QuickAddTimer> {
   final hourEntryController = new TextEditingController();
   final minuteEntryController = new TextEditingController();
   final secondsEntryController = new TextEditingController();
+  final labelEntryController = new TextEditingController();
+  String groupName;
 
   @override
   Widget build(BuildContext context) {
@@ -24,105 +26,158 @@ class QuickAddTimerState extends State<QuickAddTimer> {
     return SliverPersistentHeader(
       pinned: true,
       delegate: SliverAppBarDelegate(
-        minHeight: 75.0,
-        maxHeight: 75.0,
+        minHeight: 125.0,
+        maxHeight: 125.0,
         child: Container(
           color: Colors.blue[50],
           padding: EdgeInsets.fromLTRB(15, 20, 20, 15),
-          child: Center(
-            child: Form(
-              key: _formKey,
-              child: FocusScope(
-                node: _node,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 60,
-                      child: TextFormField(
-                        controller: hourEntryController,
-                        // maxLength: 2,
-                        // maxLengthEnforced: true,
-                        decoration: const InputDecoration(
+          child: Form(
+            key: _formKey,
+            child: FocusScope(
+              node: _node,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    width: 60,
+                    child: TextFormField(
+                      controller: hourEntryController,
+                      // maxLength: 2,
+                      // maxLengthEnforced: true,
+                      decoration: const InputDecoration(
                           labelText: 'hr',
                           border: OutlineInputBorder(),
-                          // errorText: validationService.hours.error;
-                        ),
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly
-                        ],
-                        keyboardType: TextInputType.number,
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator: (value) {
-                          return validateHours(value);
-                        },
-                        onChanged: (value) => autoAdvance(value),
-                        onFieldSubmitted: (value) => createNewTimer(value),
-                      ),
+                          errorStyle: TextStyle(height: 0)),
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      keyboardType: TextInputType.number,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) {
+                        return validateHours(value);
+                      },
+                      onChanged: (value) => autoAdvance(value),
+                      onFieldSubmitted: (value) => createNewTimer(value),
                     ),
-                    SizedBox(width: 10),
-                    Container(
-                      width: 60,
-                      child: TextFormField(
-                        controller: minuteEntryController,
-                        // maxLength: 2,
-                        // maxLengthEnforced: true,
-                        decoration: const InputDecoration(
-                          labelText: 'min',
-                          border: OutlineInputBorder(),
-                          // errorText: validationService.hours.error;
-                        ),
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly
-                        ],
-                        keyboardType: TextInputType.number,
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator: (value) {
-                          return validateMinutes(value);
-                        },
-                        onChanged: (value) => autoAdvance(value),
-                        onFieldSubmitted: (value) => createNewTimer(value),
+                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    width: 60,
+                    child: TextFormField(
+                      controller: minuteEntryController,
+                      // maxLength: 2,
+                      // maxLengthEnforced: true,
+                      decoration: const InputDecoration(
+                        labelText: 'min',
+                        border: OutlineInputBorder(),
+                        errorStyle: TextStyle(height: 0),
+                        // errorText: validationService.hours.error;
                       ),
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      keyboardType: TextInputType.number,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) {
+                        return validateMinutes(value);
+                      },
+                      onChanged: (value) => autoAdvance(value),
+                      onFieldSubmitted: (value) => createNewTimer(value),
                     ),
-                    SizedBox(width: 10),
-                    Container(
-                      width: 60,
-                      child: TextFormField(
-                        controller: secondsEntryController,
-                        // maxLength: 2,
-                        // maxLengthEnforced: true,
-                        decoration: const InputDecoration(
-                          labelText: 'sec',
-                          border: OutlineInputBorder(),
-                          // errorText: validationService.hours.error;
-                        ),
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly
-                        ],
-                        keyboardType: TextInputType.number,
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator: (value) {
-                          return validateSeconds(value);
-                        },
-                        onFieldSubmitted: (value) => createNewTimer(value),
+                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    width: 60,
+                    child: TextFormField(
+                      controller: secondsEntryController,
+                      // maxLength: 2,
+                      // maxLengthEnforced: true,
+                      decoration: const InputDecoration(
+                        labelText: 'sec',
+                        border: OutlineInputBorder(),
+                        errorStyle: TextStyle(height: 0),
                       ),
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      keyboardType: TextInputType.number,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) {
+                        return validateSeconds(value);
+                      },
+                      onFieldSubmitted: (value) => createNewTimer(value),
                     ),
-                    SizedBox(width: 20),
-                    Container(
-                      width: 50,
-                      child: Center(
-                        child: RaisedButton(
-                          child: Text("ADD"),
-                          onPressed: () {
-                            createNewTimerButton();
-                          },
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)),
+                  ),
+                  SizedBox(width: 20),
+                  Container(
+                    width: 60,
+                    height: 55,
+                    // padding: EdgeInsets.only(top: 5),
+                    child: RaisedButton(
+                      child: Text("ADD"),
+                      onPressed: () {
+                        createNewTimerButton();
+                      },
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Container(
+                    width: 150,
+                    child: TextFormField(
+                      controller: labelEntryController,
+                      maxLength: 30,
+                      maxLengthEnforced: true,
+                      decoration: const InputDecoration(
+                        labelText: 'Label',
+                        hintText: '[Optional]',
+                        border: OutlineInputBorder(),
+                        errorStyle: TextStyle(height: 0),
+                        floatingLabelBehavior: FloatingLabelBehavior.always,
+                        // errorText: validationService.hours.error;
+                      ),
+                      // inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      keyboardType: TextInputType.text,
+                      // autovalidateMode: AutovalidateMode.onUserInteraction,
+                      // validator: (value) {
+                      //   return validateLabel(value);
+                      // },
+                      onFieldSubmitted: (value) => createNewTimer(value),
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Container(
+                    width: 250,
+                    // padding: EdgeInsets.all(4),
+                    child: DropdownButtonFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Group',
+                        hintText: '[Optional]',
+                        border: const OutlineInputBorder(),
+                        errorStyle: TextStyle(height: 0),
+                        floatingLabelBehavior: FloatingLabelBehavior.always,
+                        prefixIcon: Container(
+                          padding: EdgeInsets.only(left: 10, right: 20),
+                          width: 80,
+                          child: RaisedButton(
+                            padding: const EdgeInsets.symmetric(horizontal: 5),
+                            child: Icon(Icons.add_rounded),
+                            // shape: CircleBorder(),
+                            onPressed: () {
+                              addGroup();
+                            },
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                          ),
                         ),
                       ),
+                      // autovalidateMode: AutovalidateMode.onUserInteraction,
+                      // validator: (value) {
+                      //   return validateGroupName(value);
+                      // },
+                      items: [
+                        DropdownMenuItem(
+                            child: Text("Default"), value: "Default")
+                      ],
+                      onChanged: (value) => groupName = value,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
@@ -198,5 +253,29 @@ class QuickAddTimerState extends State<QuickAddTimer> {
     } else {
       return "Enter an int";
     }
+  }
+
+  String validateLabel(String value) {
+    if (value != null) {
+      return null;
+    } else {
+      return "Enter an int";
+    }
+  }
+
+  String validateGroupName(String value) {
+    int intValue = int.tryParse(value);
+    if (intValue != null) {
+      return null;
+    } else if (hourEntryController.text.length > 0 ||
+        minuteEntryController.text.length > 0) {
+      return null;
+    } else {
+      return "Enter an int";
+    }
+  }
+
+  void addGroup() {
+    print("adding group name");
   }
 }
